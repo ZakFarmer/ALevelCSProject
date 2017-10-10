3c42b7c9c3a7a3479896d2c873373b5a5339923f
ZakFarmer <zakdoesgaming@gmail.com>
2017-10-10 13:40:15 +0100

    Created dynamic URLs for the profile view and added Devise input sanitization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # Call method for configured parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Protect the database, while allowing these fields to be updated by the app.
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,12 @@ class PagesController < ApplicationController
   end
 
   def profile
+    # Get the username from the user specified in the URL
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice=> "User '" + params[:id] + "' not found."
+    end
   end
 
   def explore
