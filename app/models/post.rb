18431049140f38a8e56a10a459fd2b814f5d7b7a
ZakFarmer <zakdoesgaming@gmail.com>
2017-10-10 14:36:42 +0100

    Fixed Devise input sanitation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 128 }
-  default_scope -> { order(created_at: desc) } # Order newest posts first
+  default_scope -> { order(created_at: :desc) } # Order newest posts first
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts
   # Define index as the root URL
   root 'pages#index'
 
